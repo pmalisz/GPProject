81616dc1410670a5be14e31da9f70877e93b3011
pmalisz <pmalisz@student.agh.edu.pl>
2022-10-30 18:04:36 +0100

    init

---
+++ src/main/java/gp/project/App.java
@@ -0,0 +1,11 @@
+package gp.project;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
