33aee9d5346f6d81fde2ff156d10340509b74d2d
pmalisz <pmalisz@student.agh.edu.pl>
2022-10-30 18:03:59 +0100

    init

---
+++ src/main/antlr4/gp/project/gen/GrammarBaseVisitor.java
@@ -0,0 +1,91 @@
+// Generated from C:/Users/Pawel/IdeaProjects/GPProject/src/main/antlr4/gp/project\Grammar.g4 by ANTLR 4.9.2
+package gp.project.gen;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link GrammarVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class GrammarBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements GrammarVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProgram(GrammarParser.ProgramContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitStatement(GrammarParser.StatementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSimple_statement(GrammarParser.Simple_statementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVariable_declaration(GrammarParser.Variable_declarationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIo_functions(GrammarParser.Io_functionsContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitBlock_statement(GrammarParser.Block_statementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitConditional_statement(GrammarParser.Conditional_statementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIteration_statement(GrammarParser.Iteration_statementContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpression(GrammarParser.ExpressionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitEquation(GrammarParser.EquationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFactor(GrammarParser.FactorContext ctx) { return visitChildren(ctx); }
+}
---
+++ src/main/antlr4/gp/project/gen/GrammarVisitor.java
@@ -0,0 +1,79 @@
+// Generated from C:/Users/Pawel/IdeaProjects/GPProject/src/main/antlr4/gp/project\Grammar.g4 by ANTLR 4.9.2
+package gp.project.gen;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link GrammarParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface GrammarVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#program}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProgram(GrammarParser.ProgramContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#statement()}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStatement(GrammarParser.StatementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#simple_statement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSimple_statement(GrammarParser.Simple_statementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#variable_declaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariable_declaration(GrammarParser.Variable_declarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#io_functions}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIo_functions(GrammarParser.Io_functionsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#block_statement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBlock_statement(GrammarParser.Block_statementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#conditional_statement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitConditional_statement(GrammarParser.Conditional_statementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#iteration_statement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIteration_statement(GrammarParser.Iteration_statementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#expression()}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpression(GrammarParser.ExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#equation()}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEquation(GrammarParser.EquationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link GrammarParser#factor}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFactor(GrammarParser.FactorContext ctx);
+}
